c9f7fc7954e3672e7505da9caa295b10d1d0ebeb
Marcos <marcosherreroa@gmail.com>
2024-06-29 15:46:23 +0200

    Basic restaurant website

---
+++ restaurant/inc/header.inc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/simple.css" />
+    <link rel="stylesheet" href="./styles/custom.css" />
+    <title>Little Bistro &bull; <?php echo $title ?></title>
+</head>
+<body>
+  <header class="header-with-background" style="background-image: url('images/<?php echo $headerImage?>'); ">
+    <h1>Little Bistro</h1>
+    <p>A cozy culinary delight</p>
+    <nav>
+      <a href="our-mission.php">Our mission</a>
+      <a href="ingredients.php">Ingredients</a>
+      <a href="menu.php">Menu</a>
+    </nav>
+  </header>
+
+  <main>
---
+++ restaurant/ingredients.php
@@ -0,0 +1,27 @@
+<?php 
+$title = 'Ingredients';
+$headerImage = 'ingredients-img-1.jpeg';
+include 'inc/header.inc.php' ;
+?>
+
+    <h1>Our ingredients</h1>
+    <p>
+    At Little Bistro, we pride ourselves on using only the finest, locally-sourced ingredients to create our dishes. 
+    Our commitment to quality begins with our partnerships with nearby farmers and artisans, ensuring that each ingredient 
+    is fresh, flavorful, and sustainably produced. From crisp vegetables to succulent meats, every component of our menu 
+    is selected with care to provide our guests with a dining experience that is both delicious and environmentally responsible.
+    </p>
+    <figure style="text-align: center;">
+      <img style="max-width: 20rem;" 
+           src="images/ingredients-img-2.jpeg" 
+           alt="Our amazing restaurant" />
+      <figcaption>Our amazing restaurant</figcaption>
+    </figure>
+    <p>
+    Our dedication to exceptional ingredients extends to our house-made sauces, dressings, and desserts, all crafted from scratch 
+    to highlight the natural flavors and textures of our produce. By focusing on seasonality, we are able to offer a dynamic menu 
+    that evolves throughout the year, celebrating the best that each season has to offer. At Little Bistro, we believe that great 
+    food starts with great ingredients, and we are passionate about bringing the very best to your table.
+    </p>
+
+<?php include 'inc/footer.inc.php'; ?>
